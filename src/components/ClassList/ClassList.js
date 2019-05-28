618d2d31e18a541df942de67b4964f7a49b9b7e1
josh <jjhouston24@gmail.com>
2019-05-28 14:32:19 -0500

    added routes

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(results => {
+        this.setState({
+          students: results.data
+        })
+      })
   }
 
   render() {
