c3c0775a69a4635b2b35b791ddbf65500bdef9d0
josh <jjhouston24@gmail.com>
2019-05-28 16:37:44 -0500

    new routes

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
   constructor() {
@@ -20,11 +21,16 @@ export default class ClassList extends Component {
   }
 
   render() {
+    const students = this.state.students.map((student, i) => (
+      <Link to={`/student/${student.id}`} key={i}>
+        <h3>{student.first_name} {student.last_name}</h3>
+      </Link>
+    ))
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {students}
       </div>
     )
   }
